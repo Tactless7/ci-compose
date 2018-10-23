be3f2dc8400580a34447117f4fd16d3d3e76009d
Juliane Blier <juliane@schoolmouv.fr>
2018-10-23 11:58:08 +0200

    Updated .yml

--- tests.test.js
+++ tests.test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
 test('go to root page', async () => {
-  let res = await axios.get('http://localhost:3000');
+  let res = await axios.get('http://ci-compose:3030');
   expect(res.data).toEqual('Hello copain');
 });
